322c262ab7f89eb410cbde1bec886e5dc04a42e8
Str3ltsov <andtaress@live.com>
2024-03-06 19:21:55 +0200

    News by countries page design

---
+++ app/Http/Controllers/CountryNewsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\CountryService;
+use App\Services\LinkService;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Throwable;
+
+class CountryNewsController extends Controller
+{
+    public function __construct(
+        private LinkService $linkService,
+        private CountryService $countryService
+    ) {
+    }
+
+    public function index(): Renderable|RedirectResponse
+    {
+        try {
+            $link = $this->linkService
+                ->getLinkByAttribute('link', '/' . Route::current()->uri);
+
+            return view('country_news.index')
+                ->with([
+                    'link' => $link,
+                    'webData' => $link->webData,
+                    'regions' => $this->countryService
+                        ->getCountriesByAttribute('parent_id', null)
+                ]);
+        } catch (Throwable $throwable) {
+            if (config('app.env') !== 'production')
+                throw $throwable;
+            else
+                return back()->with('error', $throwable->getMessage());
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryNewsController;
+use App\Http\Controllers\CountryNewsController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MagazineController;
 use Illuminate\Support\Facades\Route;
@@ -18,11 +19,14 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+// Home
 Route::get('/', [HomeController::class, 'index'])->name('home');
+// News by categories
 Route::get('/news', [CategoryNewsController::class, 'index'])->name('news');
 Route::get('/news/{category}', [CategoryNewsController::class, 'show'])->name('newsByCategory');
+// Magazines
 Route::get('/magazines', [MagazineController::class, 'index'])->name('magazines');
-Route::get('/magazines/{category}', [MagazineController::class, 'magazinesByCategory'])
-    ->name('magazinesByCategory');
-Route::get('/magazines/{category}/{subcategory}', [MagazineController::class, 'magazinesBySubcategory'])
-    ->name('magazinesBySubcategory');
+Route::get('/magazines/{category}', [MagazineController::class, 'magazinesByCategory'])->name('magazinesByCategory');
+Route::get('/magazines/{category}/{subcategory}', [MagazineController::class, 'magazinesBySubcategory'])->name('magazinesBySubcategory');
+// News by countries
+Route::get('/countries', [CountryNewsController::class, 'index'])->name('countries');
